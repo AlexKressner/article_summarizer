35ea2be26967987c9fd8d4fab382b5b25551b95e
Alex Kressner <alexkressner@Alexs-MacBook-Air.local>
2020-10-02 20:15:16 +0200

    added first routes

---
+++ project/app/api/crud.py
@@ -0,0 +1,14 @@
+# project/app/api/crud.py
+# does all the heavy lifting with the postgres database 
+
+from app.models.pydantic import SummaryPayloadSchema
+from app.models.tortoise import TextSummary
+
+
+async def post(payload: SummaryPayloadSchema) -> int:
+    summary = TextSummary(
+        url=payload.url,
+        summary="dummy summary",
+    )
+    await summary.save()
+    return summary.id
